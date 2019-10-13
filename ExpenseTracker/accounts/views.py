c270e88b8ea90b33b919d96f3b6f1f209a6dfbb4
Usman Ali <usmanusman136@hotmail.com>
2019-10-13 18:10:56 +0500

    base tempate and accounts app started

---
+++ ExpenseTracker/accounts/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request,'base.html')
+
+def register(request):
+    return
+
+def logout(request):
+    return
+
+
