a0cd49aadf7d1e66803d557b499dc9bf3d4883d8
Usman Ali <usmanusman136@hotmail.com>
2019-10-14 17:16:10 +0500

    models for expenses added and homepage template

--- ExpenseTracker/expense/admin.py
+++ ExpenseTracker/expense/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Expenses,Catagory
 # Register your models here.
+
+admin.site.register(Expenses)
+admin.site.register(Catagory)
--- ExpenseTracker/expense/models.py
+++ ExpenseTracker/expense/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from  django.contrib.auth.models import User
+from datetime import datetime
 
 # Create your models here.
+class Expenses(models.Model):
+    User_ID = models.ForeignKey(User, on_delete=models.CASCADE)
+    Amount = models.IntegerField(blank=False)
+    Catagory = models.CharField(max_length=200 , blank=False)
+    Contents=models.TextField(max_length=200)
+    Notes=models.TextField(max_length=200)
+    Date_Time = models.DateTimeField(default=datetime.now,blank =True)
+    def __str__(self):
+        return self.Catagory
+
+class Catagory(models.Model):
+    User_ID = models.IntegerField(blank=False, default=0)
+    Name = models.CharField(max_length=200)
+    def __str__(self):
+        return self.Name
