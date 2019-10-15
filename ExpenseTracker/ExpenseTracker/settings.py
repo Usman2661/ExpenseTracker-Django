d783c9c8efe2b7bb918a09bd9bb610b301777569
Usman Ali <usmanusman136@hotmail.com>
2019-10-16 00:20:47 +0500

    models for friends

--- ExpenseTracker/ExpenseTracker/settings.py
+++ ExpenseTracker/ExpenseTracker/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'accounts.apps.AccountsConfig',
     'expense.apps.ExpenseConfig',
+    'Friends.apps.FriendsConfig',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
--- ExpenseTracker/Friends/models.py
+++ ExpenseTracker/Friends/models.py
@@ -4,17 +4,16 @@ from datetime import datetime
 
 # Create your models here.
 class Request(models.Model):
-    SentBy = models.ForeignKey(User, on_delete=models.CASCADE)
-    SentTo = models.ForeignKey(User, on_delete=models.CASCADE)
-    Status = models.BooleanField(initial=False)
+    SentBy = models.ForeignKey(User, on_delete=models.CASCADE, related_name='UserID_SentBy')
+    SentTo = models.ForeignKey(User, on_delete=models.CASCADE, related_name='UserID_SentTo')
+    Status = models.BooleanField(default=False)
     DateTimeSent = models.DateTimeField(default=datetime.now,blank =True)
     def __str__(self):
         return self.Catagory
 
 class Friend(models.Model):
-    MyID = models.ForeignKey(User, on_delete=models.CASCADE)
-    FriendID =  models.ForeignKey(User, on_delete=models.CASCADE)
+    UserID = models.ForeignKey(User, on_delete=models.CASCADE, related_name='UserID_MyID')
+    FriendID =  models.ForeignKey(User, on_delete=models.CASCADE, related_name='UserID_FriendID')
     FriendSince = models.DateTimeField(default=datetime.now,blank =True)
-
     def __str__(self):
         return self.MyID
