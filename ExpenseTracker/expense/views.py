2a5ef0efaf7f0602ba25e89c0bb4c1e100067538
Usman Ali <usmanusman136@hotmail.com>
2019-10-15 15:03:21 +0500

    submit user expenses also show them in the list

--- ExpenseTracker/expense/views.py
+++ ExpenseTracker/expense/views.py
@@ -1,6 +1,42 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from expense.models import Catagory,Expenses
+from django.contrib import messages
+from django.contrib.auth.models import User,auth
+from django.contrib.auth import authenticate
+from django.db import IntegrityError
+from django.shortcuts import render_to_response
 # Create your views here.
 
 def index(request):
-    return render(request,'expense/home.html')
+
+    if request.user.is_authenticated:
+
+        if request.method=='POST':
+            amount=request.POST['amount']
+            catagories=request.POST['catagory']
+            content=request.POST['content']
+            notes=request.POST['notes']
+            UserID=request.user.id
+
+            try:
+
+                expense = Expenses.objects.create(Amount=amount, Catagory=catagories, Contents=content,Notes=notes,User_ID_id=UserID)
+            # status=user.save()
+            except IntegrityError:
+                messages.error(request,'Error: There has been an error while adding expense')
+                return redirect('home')
+            else:
+                messages.success(request,'Expense Added Succesfully')
+                return redirect('home')
+        else:
+            UserID=request.user.id
+            MyCatagories = Catagory.objects.all()
+            MyExpense = Expenses.objects.all().filter(User_ID_id=UserID)
+            context={
+                "MyCatagories":MyCatagories,
+                "MyExpense":MyExpense,
+            }
+            return render(request,'expense/home.html',context)
+
+    else:
+        return redirect("login")
